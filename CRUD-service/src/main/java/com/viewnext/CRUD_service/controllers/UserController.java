622c5079c9a226f710fd8776b976656ea2fa1607
anavarr <milumarchena@gmail.com>
2024-04-29 17:16:33 +0200

    Nuevos metodos

--- CRUD-service/src/main/java/com/viewnext/CRUD_service/controllers/UserController.java
+++ CRUD-service/src/main/java/com/viewnext/CRUD_service/controllers/UserController.java
@@ -36,21 +36,6 @@ public class UserController {
 
 
     /**
-     * HOLA
-     * Consultar un usuario por su correo.
-     *
-     * @param correo Correo del usuario.
-     * @return ResponseEntity con el objeto UserDto.
-     */
-    @GetMapping("/{correo}")
-    public ResponseEntity<UserDto> consultarUsuario(
-            @PathVariable String correo) {
-        UserDto userDto = userServiceI.consultarUsuario(correo);
-        return ResponseEntity.ok(userDto);
-    }
-
-
-    /**
      * Edita los datos de un usuario.
      *
      * @param correo Correo del usuario.
@@ -74,6 +59,84 @@ public class UserController {
     @DeleteMapping("borrar/{correo}")
     public void borrarUsuarioPorEmail(@PathVariable String correo) {
         userServiceI.borrarUsuarioPorEmail(correo);
+    }
+
+    /**
+     * Consultar un usuario por su correo.
+     *
+     * @param nombre Nombre del usuario.
+     * @return ResponseEntity con el objeto UserDto.
+     */
+    @GetMapping("/nombre/{nombre}")
+    public ResponseEntity<UserDto> consultarUsuarioPorNombre(
+            @PathVariable String nombre) {
+        UserDto userDto = userServiceI.consultarUsuarioPorNombre(nombre);
+        return ResponseEntity.ok(userDto);
+    }
+
+    /**
+     * Consultar un usuario por su correo.
+     *
+     * @param apellidos Apellidos del usuario.
+     * @return ResponseEntity con el objeto UserDto.
+     */
+    @GetMapping("/apellidos/{apellidos}")
+    public ResponseEntity<UserDto> consultarUsuarioPorApellidos(
+            @PathVariable String apellidos) {
+        UserDto userDto = userServiceI.consultarUsuarioPorApellidos(apellidos);
+        return ResponseEntity.ok(userDto);
+    }
+
+    /**
+     * Consultar un usuario por su correo.
+     *
+     * @param edad Edad del usuario.
+     * @return ResponseEntity con el objeto UserDto.
+     */
+    @GetMapping("/edad/{edad}")
+    public ResponseEntity<UserDto> consultarUsuarioPorEdad(
+            @PathVariable int edad) {
+        UserDto userDto = userServiceI.consultarUsuarioPorEdad(edad);
+        return ResponseEntity.ok(userDto);
+    }
+
+    /**
+     * Consultar un usuario por su correo.
+     *
+     * @param correo Correo del usuario.
+     * @return ResponseEntity con el objeto UserDto.
+     */
+    @GetMapping("/correo/{correo}")
+    public ResponseEntity<UserDto> consultarUsuarioPorCorreo(
+            @PathVariable String correo) {
+        UserDto userDto = userServiceI.consultarUsuarioPorCorreo(correo);
+        return ResponseEntity.ok(userDto);
+    }
+
+    /**
+     * Consultar un usuario por su correo.
+     *
+     * @param direccion Direccion del usuario.
+     * @return ResponseEntity con el objeto UserDto.
+     */
+    @GetMapping("/direccion/{direccion}")
+    public ResponseEntity<UserDto> consultarUsuarioPorDireccion(
+            @PathVariable String direccion) {
+        UserDto userDto = userServiceI.consultarUsuarioPorDireccion(direccion);
+        return ResponseEntity.ok(userDto);
+    }
+
+    /**
+     * Consultar un usuario por su correo.
+     *
+     * @param telefono Telefono del usuario.
+     * @return ResponseEntity con el objeto UserDto.
+     */
+    @GetMapping("/telefono/{telefono}")
+    public ResponseEntity<UserDto> consultarUsuarioPorTelefono(
+            @PathVariable int telefono) {
+        UserDto userDto = userServiceI.consultarUsuarioPorTelefono(telefono);
+        return ResponseEntity.ok(userDto);
     }
 
 }
