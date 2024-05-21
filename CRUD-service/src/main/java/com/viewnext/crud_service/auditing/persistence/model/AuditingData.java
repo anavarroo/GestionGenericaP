840059563addea44012206176856aa8d35f7331f
Alejandro Ramirez <alejandro.ramirez@viewnext.com>
2024-05-21 15:39:32 +0200

    fixed bugs

--- CRUD-service/src/main/java/com/viewnext/crud_service/auditing/persistence/model/AuditingData.java
+++ CRUD-service/src/main/java/com/viewnext/crud_service/auditing/persistence/model/AuditingData.java
@@ -1,9 +1,6 @@
 package com.viewnext.crud_service.auditing.persistence.model;
 
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
+import jakarta.persistence.*;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
@@ -15,18 +12,23 @@ import java.util.Date;
 @Data
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@Entity(name = "Audit-Users")
 public class AuditingData {
 
+    @Column(name = "Created_by")
     @CreatedBy
     private String createdBy;
 
+    @Column(name = "Created_Date")
     @CreatedDate
     @Temporal(TemporalType.TIMESTAMP)
     private Date createdDate;
 
+    @Column(name = "Last_Modified_By")
     @LastModifiedBy
     private String lastModifiedBy;
 
+    @Column(name = "Last_Modified_Date")
     @LastModifiedBy
     @Temporal(TemporalType.TIMESTAMP)
     private Date lastModifiedDate;
