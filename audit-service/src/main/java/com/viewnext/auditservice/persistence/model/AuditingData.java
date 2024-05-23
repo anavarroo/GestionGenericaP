83ba3d94c12ec3324200c83ce67b9135d9125a4b
Alejandro Ramirez <alejandro.ramirez@viewnext.com>
2024-05-23 08:49:54 +0200

    Impement Audit-service in Eureka Server

--- audit-service/src/main/java/com/viewnext/auditservice/persistence/model/AuditingData.java
+++ audit-service/src/main/java/com/viewnext/auditservice/persistence/model/AuditingData.java
@@ -2,16 +2,9 @@ package com.viewnext.auditservice.persistence.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import java.io.Serializable;
 import java.util.Date;
 
@@ -36,9 +29,4 @@ public class AuditingData implements Serializable {
 
     @Column(name = "EndPoint")
     private String typeRequest;
-
-    public AuditingData(String createdBy, String typeRequest) {
-        this.createdBy = createdBy;
-        this.typeRequest = typeRequest;
-    }
 }
