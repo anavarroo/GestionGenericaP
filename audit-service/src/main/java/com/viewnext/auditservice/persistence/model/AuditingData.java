5b5842e26b50b6e8163fe374a54f328faebad4c2
Alejandro Ramirez <alejandro.ramirez@viewnext.com>
2024-05-23 09:17:22 +0200

    Fixed bugs

--- audit-service/src/main/java/com/viewnext/auditservice/persistence/model/AuditingData.java
+++ audit-service/src/main/java/com/viewnext/auditservice/persistence/model/AuditingData.java
@@ -1,6 +1,5 @@
 package com.viewnext.auditservice.persistence.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,14 +20,13 @@ public class AuditingData implements Serializable {
     @Column(name = "id")
     private long id;
 
-    @Column(name = "Author", nullable = false)
+    @Column(name = "Author")
     private String createdBy;
 
+    @Column(name = "Created_Date")
     @CreationTimestamp
-    @Column(name = "create_date", nullable = false, updatable = false)
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone="Europe/Zagreb")
     private Date createdDate;
 
-    @Column(name = "EndPoint", nullable = false)
+    @Column(name = "EndPoint")
     private String typeRequest;
 }
