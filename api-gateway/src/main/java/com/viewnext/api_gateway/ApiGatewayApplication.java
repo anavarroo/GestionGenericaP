5e405f155315ea9bb9c1c81f9d1cd29f3b651ad3
anavarr <milumarchena@gmail.com>
2024-05-23 10:02:52 +0200

    Correción docker file API Gateway para Aspecto hora Europea v3

--- api-gateway/src/main/java/com/viewnext/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/viewnext/api_gateway/ApiGatewayApplication.java
@@ -1,8 +1,11 @@
 package com.viewnext.api_gateway;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +15,8 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Madrid"));
+	}
 }
