4cf3ba0389f243a18b1c2db798c1522dc56a9fc2
Alejandro Ramirez <alejandro.ramirez@viewnext.com>
2024-05-27 13:10:29 +0200

    fixed bugs Rabbitmq v.0.2

--- audit-service/src/main/java/com/viewnext/auditservice/services/AuditingServiceI.java
+++ audit-service/src/main/java/com/viewnext/auditservice/services/AuditingServiceI.java
@@ -1,6 +1,5 @@
 package com.viewnext.auditservice.services;
 
-import com.viewnext.auditservice.persistence.AuditingDataDto;
 import com.viewnext.auditservice.persistence.model.AuditingData;
 
 public interface AuditingServiceI {
