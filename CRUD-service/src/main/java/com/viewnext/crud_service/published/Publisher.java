73ba535ec6693e87858bc8f88c1d897f7fbcb4da
Alejandro Ramirez <alejandro.ramirez@viewnext.com>
2024-05-27 10:29:22 +0200

    fixed bugs Rabbitmq

--- CRUD-service/src/main/java/com/viewnext/crud_service/published/Publisher.java
+++ CRUD-service/src/main/java/com/viewnext/crud_service/published/Publisher.java
@@ -4,6 +4,7 @@ import com.viewnext.crud_service.persistence.dto.AuditingDataDto;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,6 +14,7 @@ public class Publisher {
     private final RabbitTemplate template;
     private final Queue queue;
 
+    @Autowired
     public Publisher(RabbitTemplate template, Queue queue) {
         this.template = template;
         this.queue = queue;
