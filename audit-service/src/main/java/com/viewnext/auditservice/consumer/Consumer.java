39b3e9e815c1f3db5237a3f16a0e8453c757c0d6
Alejandro Ramirez <alejandro.ramirez@viewnext.com>
2024-05-27 12:07:32 +0200

    fixed bugs Rabbitmq

--- audit-service/src/main/java/com/viewnext/auditservice/consumer/Consumer.java
+++ audit-service/src/main/java/com/viewnext/auditservice/consumer/Consumer.java
@@ -1,8 +1,10 @@
 package com.viewnext.auditservice.consumer;
 
 import com.viewnext.auditservice.persistence.model.AuditingData;
+import com.viewnext.auditservice.services.AuditingServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
@@ -10,9 +12,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class Consumer {
 
+    private final AuditingServiceImpl auditingService;
+
+    @Autowired
+    public Consumer(AuditingServiceImpl auditingService) {
+        this.auditingService = auditingService;
+    }
+
     @RabbitListener(queues = { "${generics.queue.name} "})
     public void receive(@Payload AuditingData message) {
-        log.info("Received message: " + message);
+        log.info("Received message: {}", message);
+        auditingService.saveAudit(message);
 
     }
 
