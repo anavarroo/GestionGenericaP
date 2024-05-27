deda7d712752c4b7ce100490ec13c8a7e3afa6ac
Alejandro Ramirez <alejandro.ramirez@viewnext.com>
2024-05-27 17:03:09 +0200

    fixed bugs Rabbitmq v.0.7

---
+++ audit-service/src/main/java/com/viewnext/auditservice/consumer/RabbitMQConsumer.java
@@ -0,0 +1,18 @@
+package com.viewnext.auditservice.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQConsumer.class);
+
+    @RabbitListener(queues = {"${rabbitmq.queue.name}"})
+    public void consume(String message) {
+        LOGGER.info(String.format("Recieved message -> %s", message));
+    }
+
+}
