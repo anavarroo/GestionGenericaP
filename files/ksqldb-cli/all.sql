c35f91b4f87d88e3207d9ee154b24716ace9a498
anavarr <milumarchena@gmail.com>
2024-06-03 15:54:33 +0200

    Cambios v6

--- files/ksqldb-cli/all.sql
+++ files/ksqldb-cli/all.sql
@@ -11,11 +11,53 @@ WITH (
   'key'='id'
 );
 
-CREATE SINK CONNECTOR redis_sink WITH (
-  'connector.class'='com.github.jcustenborder.kafka.connect.redis.RedisSinkConnector',
-  'tasks.max'='1',
-  'topics'='usuarios',
-  'redis.hosts'='redis:6379',
-  'key.converter'='org.apache.kafka.connect.converters.ByteArrayConverter',
-  'value.converter'='org.apache.kafka.connect.converters.ByteArrayConverter'
+CREATE STREAM usuarios_raw WITH (
+    KAFKA_TOPIC='usuarios',
+    VALUE_FORMAT='AVRO'
 );
+
+CREATE STREAM usuarios_cleaned WITH (
+    KAFKA_TOPIC='usuarios_cleaned',
+    VALUE_FORMAT='AVRO'
+) AS
+SELECT
+    id,
+    apellidos,
+    REPLACE(contrasena, '\x00', '') AS contrasena,
+    correo,
+    create_date,
+    direccion,
+    edad,
+    edition_date,
+    estado,
+    mfa_enabled,
+    nombre,
+    role,
+    fakey,
+    telefono
+FROM usuarios_raw;
+
+
+CREATE SINK CONNECTOR redis_sink WITH (
+{
+  "name": "redis_sink",
+  "config": {
+    "connector.class": "com.github.jcustenborder.kafka.connect.redis.RedisSinkConnector",
+    "tasks.max": "1",
+    "topics": "usuarios_cleaned",
+    "redis.hosts": "redis:6379",
+    "key.converter": "org.apache.kafka.connect.converters.ByteArrayConverter",
+    "value.converter": "org.apache.kafka.connect.converters.ByteArrayConverter"
+  }
+});
+
+
+-- CREATE SINK CONNECTOR redis_sink WITH (
+--  'connector.class'='com.github.jcustenborder.kafka.connect.redis.RedisSinkConnector',
+--  'tasks.max'='1',
+--  'topics'='usuarios',
+--  'redis.hosts'='redis:6379',
+--  'key.converter'='org.apache.kafka.connect.converters.ByteArrayConverter',
+--  'value.converter'='org.apache.kafka.connect.converters.ByteArrayConverter'
+--);
+
