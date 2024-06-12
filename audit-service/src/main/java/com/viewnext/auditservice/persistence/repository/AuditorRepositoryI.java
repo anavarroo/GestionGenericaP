9434cd1f5e6572c29f3e41da34b7ba8edde9600c
Alejandro Ramirez <alejandro.ramirez@viewnext.com>
2024-06-12 09:06:31 +0200

    fixed bugs

--- audit-service/src/main/java/com/viewnext/auditservice/persistence/repository/AuditorRepositoryI.java
+++
@@ -1,7 +0,0 @@
-package com.viewnext.auditservice.persistence.repository;
-
-import com.viewnext.crud_service.persistence.model.AuditingData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuditorRepositoryI extends JpaRepository<AuditingData, Long> {
-}
--- audit-service/src/main/java/com/viewnext/auditservice/services/AuditingServiceI.java
+++
@@ -1,9 +0,0 @@
-package com.viewnext.auditservice.services;
-
-import com.viewnext.crud_service.persistence.model.AuditingData;
-
-public interface AuditingServiceI {
-
-    public AuditingData saveAudit(AuditingData auditingData);
-
-}
